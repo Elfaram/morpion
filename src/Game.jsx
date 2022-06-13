da54622d0abb265fd20fedef89cc8eb948220e20
[Lille-JS] Vaentin Nav <riverpure59@hotmail.fr>
2022-06-13 14:54:29 +0200

    save

--- src/Game.jsx
+++ src/Game.jsx
@@ -2,6 +2,30 @@ import React from 'react';
 import Board from './Board';
 
 class Game extends React.Component {
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   constructor(props) {
     super(props);
     this.state = {
@@ -14,31 +38,12 @@ class Game extends React.Component {
       xIsNext: true,
     };
   }
-  // const calculateWinner(squares) {
-  //   const lines = [
-  //     [0, 1, 2],
-  //     [3, 4, 5],
-  //     [6, 7, 8],
-  //     [0, 3, 6],
-  //     [1, 4, 7],
-  //     [2, 5, 8],
-  //     [0, 4, 8],
-  //     [2, 4, 6],
-  //   ];
-  //   for (let i = 0; i < lines.length; i++) {
-  //     const [a, b, c] = lines[i];
-  //     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-  //       return squares[a];
-  //     }
-  //   }
-  //   return null;
-  // }
 
   handleClick(i) {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    const calculateWinner  =
+
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
